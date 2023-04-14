ced7510bd2bdea4560e2017a011fe38553b5ebbf
AdenBinFarrukh <adenbinfarrukh.fl@gmail.com>
2023-04-14 11:54:51 -0700

    Adding public private option in share

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -8,103 +8,119 @@ import { ChangeContext } from "../../context/changeContext";
 import axios from "axios";
 
 const Share = () => {
-  const [desc, setDesc] = useState("");
-  const [file, setFile] = useState(null);
+    const [desc, setDesc] = useState("");
+    const [isChecked, setIsChecked] = useState(false);
+    const [file, setFile] = useState(null);
 
-  const { currentUser } = useContext(AuthContext);
-  const { postChange, setPostChange } = useContext(ChangeContext);
+    const { currentUser } = useContext(AuthContext);
+    const { postChange, setPostChange } = useContext(ChangeContext);
 
-  const handleClick = async (e) => {
-    e.preventDefault();
-    try {
-      const postData = {
-        desc: desc,
-        userId: currentUser._id,
-      };
-      if (file) {
-        const formData = new FormData();
-        formData.append("file", file);
-        const res = await axios.post(
-          "http://localhost:8800/api/upload",
-          formData
-        );
-        postData.image = res.data.filename;
-      }
-      const res = await axios.post(
-        "http://localhost:8800/api/posts/",
-        postData
-      );
-      console.log(res.data);
-      setDesc("");
-      setFile(null);
-      setPostChange(!postChange);
-    } catch (err) {
-      console.log(err);
+    function handleCheckboxChange() {
+        setIsChecked(!isChecked);
     }
-  };
 
-  return (
-    <div className="share">
-      <div className="container">
-        <div className="top">
-          <div className="left">
-            {currentUser.profilePicture ? (
-              <img
-                src={
-                  process.env.REACT_APP_Image_Path + currentUser.profilePicture
-                }
-                alt=""
-              />
-            ) : (
-              <img
-                src={process.env.REACT_APP_Image_Path + "No_Image.jpg"}
-                alt=""
-              />
-            )}
-            <input
-              type="text"
-              placeholder={`What's on your mind ${currentUser.username}?`}
-              onChange={(e) => setDesc(e.target.value)}
-              value={desc}
-            />
-          </div>
-          <div className="right">
-            {file && (
-              <img className="file" alt="" src={URL.createObjectURL(file)} />
-            )}
-          </div>
-        </div>
-        <hr />
-        <div className="bottom">
-          <div className="left">
-            <input
-              type="file"
-              id="file"
-              style={{ display: "none" }}
-              onChange={(e) => setFile(e.target.files[0])}
-            />
-            <label htmlFor="file">
-              <div className="item">
-                <img src={Image} alt="" />
-                <span>Add Image</span>
-              </div>
-            </label>
-            <div className="item">
-              <img src={Map} alt="" />
-              <span>Add Place</span>
+    const handleClick = async (e) => {
+        e.preventDefault();
+        try {
+            const postData = {
+                desc: desc,
+                userId: currentUser._id,
+                is_public: isChecked,
+            };
+            if (file) {
+                const formData = new FormData();
+                formData.append("file", file);
+                const res = await axios.post(
+                    "http://localhost:8800/api/upload",
+                    formData
+                );
+                postData.image = res.data.filename;
+            }
+            const res = await axios.post(
+                "http://localhost:8800/api/posts/",
+                postData
+            );
+            console.log(res.data);
+            setDesc("");
+            setFile(null);
+            setPostChange(!postChange);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div className="share">
+            <div className="container">
+                <div className="top">
+                    <div className="left">
+                        {currentUser.profilePicture ? (
+                            <img
+                                src={
+                                    process.env.REACT_APP_Image_Path +
+                                    currentUser.profilePicture
+                                }
+                                alt=""
+                            />
+                        ) : (
+                            <img
+                                src={
+                                    process.env.REACT_APP_Image_Path +
+                                    "No_Image.jpg"
+                                }
+                                alt=""
+                            />
+                        )}
+                        <input
+                            type="text"
+                            placeholder={`What's on your mind ${currentUser.username}?`}
+                            onChange={(e) => setDesc(e.target.value)}
+                            value={desc}
+                        />
+                    </div>
+                    <div className="right">
+                        {file && (
+                            <img
+                                className="file"
+                                alt=""
+                                src={URL.createObjectURL(file)}
+                            />
+                        )}
+                    </div>
+                </div>
+                <hr />
+                <div className="bottom">
+                    <div className="left">
+                        <input
+                            type="file"
+                            id="file"
+                            style={{ display: "none" }}
+                            onChange={(e) => setFile(e.target.files[0])}
+                        />
+                        <label htmlFor="file">
+                            <div className="item">
+                                <img src={Image} alt="" />
+                                <span>Add Image</span>
+                            </div>
+                        </label>
+                        <label>
+                            <div className="item">
+                                <input
+                                    type="checkbox"
+                                    checked={isChecked}
+                                    onChange={handleCheckboxChange}
+                                />
+                                <span>Make Public</span>
+                            </div>
+                        </label>
+                    </div>
+                    <div className="right">
+                        <button onClick={handleClick}>Share</button>
+                    </div>
+                </div>
             </div>
-            <div className="item">
-              <img src={Friend} alt="" />
-              <span>Tag Friends</span>
-            </div>
-          </div>
-          <div className="right">
-            <button onClick={handleClick}>Share</button>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Share;
