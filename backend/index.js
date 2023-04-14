330e69cb351f808c881ee18b032a6b72eb45579b
AdenBinFarrukh <adenbinfarrukh.fl@gmail.com>
2023-04-14 13:06:17 -0700

    Adding build code

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,34 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-const cors = require('cors');
+const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../frontend/build");
+
+app.use(express.static(buildPath));
+
+app.get("/*", function (req, res) {
+    res.sendFile(
+        path.join(__dirname, "../frontend/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    );
+});
 
 dotenv.config();
 
 mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to MongoDB");
+    }
 );
 
 //middleware
@@ -28,32 +44,38 @@ app.use(morgan("common"));
 app.use(cors());
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "Public/Images");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
-  }
+    destination: (req, file, cb) => {
+        cb(null, "Public/Images");
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(
+            null,
+            file.fieldname +
+                "-" +
+                uniqueSuffix +
+                path.extname(file.originalname)
+        );
+    },
 });
 
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    res.status(200).json({ filename: req.file.filename });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Error uploading file" });
-  }
+    try {
+        res.status(200).json({ filename: req.file.filename });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Error uploading file" });
+    }
 });
 
 // define route to serve uploaded files
-app.use('/Images', express.static(path.join(__dirname, 'Public/Images')));
+app.use("/Images", express.static(path.join(__dirname, "Public/Images")));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
-  console.log("Backend server is running!");
+    console.log("Backend server is running!");
 });
