0aa522d0c15d0d2726e9fed61088102166b403e1
AdenBinFarrukh <adenbinfarrukh.fl@gmail.com>
2023-04-19 05:38:13 -0700

    Adding build code

--- backend/index.js
+++ backend/index.js
@@ -29,14 +29,7 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(cors());
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     next();
-// });
-
+//* Store Image
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "Public/Images");
